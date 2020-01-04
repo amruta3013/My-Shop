149dae62ffc35b64df691a66c32851fb7c489266
amruta3013 <59464162+amruta3013@users.noreply.github.com>
2020-01-04 14:29:01 +0530

    Product Grouping Adding

---
+++ MyShop.WebUI/Controllers/ProductCategoryManagerController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyShop.Core.Models;
+using MyShop.DataAccess.InMemory;
+
+
+
+namespace MyShop.WebUI.Controllers
+{
+    public class ProductCategoryManagerController : Controller
+    {
+
+        ProductCategoryRepository Context;
+
+        public ProductCategoryManagerController()
+        {
+            Context = new ProductCategoryRepository();
+        }
+
+        // GET: ProductCategoryManager
+        public ActionResult Index()
+        {
+            List<ProductCategory> productsCategories = Context.Collection().ToList();
+            return View(productsCategories);
+        }
+
+        public ActionResult Create()
+        {
+            ProductCategory productcategory = new ProductCategory();
+            return View(productcategory);
+        }
+        [HttpPost]
+        public ActionResult Create(ProductCategory productcategory)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(productcategory);
+            }
+            else
+            {
+                Context.Insert(productcategory);
+                Context.commit();
+
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Edit(string Id)
+        {
+            ProductCategory ProductCategory = Context.Find(Id);
+            if (ProductCategory == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(ProductCategory);
+            }
+        }
+        [HttpPost]
+        public ActionResult Edit(ProductCategory productcategory, string Id)
+        {
+            ProductCategory productcategoryToEdit = Context.Find(Id);
+
+            if (productcategoryToEdit == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(productcategory);
+                }
+                productcategoryToEdit.CategoryName = productcategory.CategoryName;
+
+
+                Context.commit();
+
+                return RedirectToAction("Index");
+
+            }
+        }
+
+
+        public ActionResult Delete(string Id)
+        {
+            ProductCategory productcategoryToDelete = Context.Find(Id);
+            if (productcategoryToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(productcategoryToDelete);
+            }
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult ConfirmDelete(string Id)
+        {
+            ProductCategory productcategoryToDelete = Context.Find(Id);
+            if (productcategoryToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                Context.Delete(Id);
+                Context.commit();
+                return RedirectToAction("Index");
+            }
+        }
+
+    }
+}
--- MyShop.WebUI/Controllers/ProductManagerController.cs
+++ MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
 using MyShop.DataAccess.InMemory;
 
 namespace MyShop.WebUI.Controllers
@@ -11,10 +12,11 @@ namespace MyShop.WebUI.Controllers
     public class ProductManagerController : Controller
     {
         ProductRepository Context;
-
+        ProductCategoryRepository productCategories;
         public ProductManagerController()
         {
             Context = new ProductRepository();
+            productCategories = new ProductCategoryRepository();
         }
 
         // GET: ProductManager
@@ -26,8 +28,11 @@ namespace MyShop.WebUI.Controllers
 
         public ActionResult Create()
         {
-            Product product = new Product();
-            return View(product);
+            ProductManagerViewModel viewModel = new ProductManagerViewModel();
+
+            viewModel.product = new Product();
+            viewModel.ProductCategories = productCategories.Collection();
+            return View(viewModel);
         }
        [HttpPost]
         public ActionResult Create(Product product)
@@ -54,7 +59,12 @@ namespace MyShop.WebUI.Controllers
             }
             else
             {
-                return View(Product);
+                ProductManagerViewModel viewModel = new ProductManagerViewModel();
+
+                viewModel.product = Product;
+                viewModel.ProductCategories = productCategories.Collection();
+
+                return View(viewModel);
             }
         }
         [HttpPost]
